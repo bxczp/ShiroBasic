a57c53645afd295016839856f95c28355120dfa2
e <bxczp@hotmail.com>
2018-06-16 19:48:38 +0800

    0616

--- src/main/java/com/bxczp/servlet/LoginServlet.java
+++ src/main/java/com/bxczp/servlet/LoginServlet.java
@@ -13,7 +13,7 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
 
-public class LoginServlet extends HttpServlet{
+public class LoginServlet extends HttpServlet {
 
     /**
      * 
@@ -22,10 +22,10 @@ public class LoginServlet extends HttpServlet{
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //轉發
+        // 轉發
         req.getRequestDispatcher("login.jsp").forward(req, resp);
-//        重定向
-//        resp.sendRedirect(req.getContextPath()+"/login.jsp");
+        // 重定向
+        // resp.sendRedirect(req.getContextPath()+"/login.jsp");
     }
 
     @Override
@@ -34,23 +34,21 @@ public class LoginServlet extends HttpServlet{
         String password = req.getParameter("password");
         Subject subject = SecurityUtils.getSubject();
         UsernamePasswordToken token = new UsernamePasswordToken(userName, password);
-        Session session=subject.getSession();
+        Session session = subject.getSession();
         try {
             subject.login(token);
-            System.out.println("sessionId:"+session.getId());
-            System.out.println("sessionHost:"+session.getHost());
-//            session的有效时间 默认是半小时
-            System.out.println("sessionTimeout:"+session.getTimeout());
+            System.out.println("sessionId:" + session.getId());
+            System.out.println("sessionHost:" + session.getHost());
+            // session的有效时间 默认是半小时
+            System.out.println("sessionTimeout:" + session.getTimeout());
             session.setAttribute("info", "session的数据");
             resp.sendRedirect("success.jsp");
         } catch (Exception e) {
             System.out.println("Login Fail");
             session.setAttribute("errorInfo", "用户名或者密码错误");
-//          重定向
-          resp.sendRedirect(req.getContextPath()+"/login");
+            // 重定向
+            resp.sendRedirect(req.getContextPath() + "/login");
         }
     }
-    
-    
 
 }
